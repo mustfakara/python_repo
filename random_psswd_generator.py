f22721e2d33f8563a380f486ad5b4aa1407f772a
Mustafa Kara <88894240+mustfakara@users.noreply.github.com>
2023-02-04 00:13:49 +0300

    Add files via upload
    Bu kod, random ve string kütüphanelerini kullanarak belirtilen uzunlukta rastgele şifreler oluşturmayı sağlar. "string.ascii_letters","string.digits" ve "string.punctuation" sabit değişkenleri, oluşturulacak şifrenin içindeki olabilecek karakterleri içermektedir. "random.choice" fonksiyonu ise bu sabitler içinden rastgele karakter seçerek string tipte şifre oluşturur. Şifre fonksiyon tarafından geri gönderilir ve ekrana yazdırılır.

---
+++ random_psswd_generator.py
@@ -0,0 +1,13 @@
+import random
+import string
+
+
+def generate_password(length):
+    chars = string.ascii_letters + string.digits + string.punctuation
+    password = "".join(random.choice(chars) for i in range(length))
+    return password
+
+
+length = int(input("Enter the desired password length: "))
+password = generate_password(length)
+print("Generated password:", password)
